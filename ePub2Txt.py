4787ed5647962a0ecc779f7752c8b9d704fb84e8
leicheng <life545@163.com>
2014-03-15 18:14:10 -0700

    parse html and save as txt file

--- ePub2Txt.py
+++ ePub2Txt.py
@@ -1,25 +1,74 @@
-import os, sys, json
+import os, sys, re
+import codecs
+import unicodedata
+from bs4 import BeautifulSoup
 
-# scan all books in the dictionary
-def scanBooks(bdir):
-	pass
+booksDir = sys.argv[1]
+txtFileDir = sys.argv[2]
 
-# scan recursively all contents files(html files) in the directory of each book
-def scanContentsForBooks(specificBdir):
-	pass
+
+# scan all the content files for all books, and return an array of the content file list
+def ScanContents(rootdir):
+	contentList = []
+	for dirname, subdirs, files in os.walk(rootdir):
+		for fname in files:
+			name, ext = os.path.splitext(fname)
+			# print name, ext
+			if ext == ".html" or ext == ".xhtml" or ext == ".htm":
+				# print os.path.join(dirname, fname)
+				contentList.append(os.path.join(dirname, fname))
+
+	# print contentList
+	return contentList
 
 # parse contents in each html file
-def parseHtml():
-	pass
+def parseContents(contentList):
+	for fullFilename in contentList:
+		# print fullFilename
+		nameList = re.findall('/(.*)\.epub', fullFilename, re.DOTALL)
+		filename = os.path.basename(nameList[0])
+		# print filename
+
+		# start to read contents from the file
+		soup = BeautifulSoup(open(fullFilename))
+		# soup.p.encode("utf-8")
+		pTagList = soup.find_all('p')
+		textList = []
+		for pTag in pTagList:
+			# if len(pTag.text) > 0:
+				# print len(pTag.text)
+				# decode_text = pTag.text.decode('utf-8')
+				# textList.append(decode_text)
+			textList.append(pTag.text)
+			# print pTag.text
+		
+		saveContents(filename+".txt", textList)
 
 # save parsed contents of each book in a specific direcoty with its genre name and ordered number
-def saveContents():
-	pass
+def saveContents(filename, contentsList):
+	fullFilename = os.path.join(txtFileDir, filename)
+	print "fullFilename", fullFilename
+
+	try:
+		fhandler = open(fullFilename, 'a')
+		for content in contentsList:
+			# content = unicode(content.strip(codecs.BOM_UTF8), 'utf-8')		
+			fhandler.write(content)
+			print content
+		fhandler.close()
+	except Exception, e:
+		raise e
 
 
 # main callee
 def main():
-	pass
+	if len(sys.argv) < 3:
+		print "........\nmiss arguments\n........"
+		return
+
+	contentList = ScanContents(booksDir)
+	parseContents(contentList)
+	
 
 # where start to wrok
 if __name__ == '__main__':
